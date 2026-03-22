7ae2f842d67a55847b168a8711f913120048d9d2
Prashanth <chotur15@gmail.com>
2026-03-22 21:45:38 +0530

    hello added new changes

---
+++ content/basics/hello.md
@@ -0,0 +1,23 @@
+---
+title: Hello
+date: 2026-03-22
+author: Your Name
+cell_count: 2
+score: 0
+---
+
+```python
+name = input("Enter your name: ")
+```
+
+    Enter your name:  Prashanth
+    
+
+
+```python
+
+```
+
+
+---
+**Score: 0**
--- pelicanconf.py
+++ pelicanconf.py
@@ -1,8 +1,8 @@
 
 
-AUTHOR          = '<your name>'  # use your name
-SITENAME        = 'title' # use title
-GITHUB_USERNAME = 'tactlabs' # use your username
+AUTHOR          = 'Prashanth'  # use your name
+SITENAME        = 'My Python Notes' # use title
+GITHUB_USERNAME = 'chotur15-oss' # use your username
 
 
 # Blogroll
@@ -12,7 +12,7 @@ LINKS = (
 
 # Social widget
 SOCIAL = (
-    ("GitHub", "https://github.com/<yourusername>/"),
+    ("GitHub", "https://github.com/chotur15-oss/"),
     ("LinkedIn", "<linkedin handle>"),
 )
 
